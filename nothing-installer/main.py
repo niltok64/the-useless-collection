b62ed295a8d1450e425e06a65e81bd181a83b183
DaStickMan <72703236+dastikman@users.noreply.github.com>
2021-11-30 16:13:12 +0000

    init commit 1

---
+++ nothing-installer/main.py
@@ -0,0 +1,10 @@
+# by mink64
+# a parody of installation wizards
+from guizero import *
+
+def install_nothing():
+	info("Installation", "Thank you for installing nothing!")
+
+app = App(title="Nothing Installer")
+install_button = PushButton(app, command=install_nothing, text="Install")
+app.display()
