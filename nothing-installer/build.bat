b780c6124b520ed687b8f2599067b941e35a0245
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-14 14:25:15 +0000

    Update build.bat

--- nothing-installer/build.bat
+++ nothing-installer/build.bat
@@ -1,7 +1,8 @@
 @ECHO OFF
 ECHO building
 pip install pyinstaller
-MKDIR done
+cd ..
+MKDIR nothing-build
 pyinstaller main.py --onefile -n nothing-installer -w --distpath done
 ECHO done building
 PAUSE
