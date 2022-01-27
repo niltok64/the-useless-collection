68332a7a3ecdd10dbbe7eeb868223c8e19d4181d
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-27 10:18:53 +0000

    Update build-all.bat

--- automated-build/build-all.bat
+++ automated-build/build-all.bat
@@ -3,13 +3,13 @@ pip install pyinstaller
 echo building...
 cd ..
 cd height-calculator
-pyinstaller main.py --onefile
+pyinstaller main.py --onefile --distpath done
 cd ..
 cd ok
-pyinstaller main.py --onefile -w
+pyinstaller main.py --onefile -w --distpath done
 cd ..
 cd nothing-installer
-pyinstaller main.py --onefile -w
+pyinstaller main.py --onefile -w --distpath done
 cd ..
 echo done building lel
 pause
